29685f065db75778c0ef0b4a8fe85a39eb7cd183
jaoleal <jgleal@protonmail.com>
2024-11-22 15:07:52 -0300

    006 done

--- submission/006.sh
+++ submission/006.sh
@@ -1,1 +1,4 @@
 # Which tx in block 257,343 spends the coinbase output of block 256,128?
+coinbasetx=$(bitcoin-cli getblock $(bitcoin-cli getblockhash  256128) | jq  --raw-output .tx[0])
+transaction=$(bitcoin-cli getblock $(bitcoin-cli getblockhash 257343) 2 | jq --arg coinbasetx "$coinbasetx" '.tx[] | select(.vin[]?.txid == $coinbasetx)')
+echo $transaction| jq --raw-output .txid
