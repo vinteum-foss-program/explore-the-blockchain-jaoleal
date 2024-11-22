b6d87b44c6295838d6333ce64f38af8b5e853446
jaoleal <jgleal@protonmail.com>
2024-11-22 11:46:15 -0300

    004 done

--- submission/004.sh
+++ submission/004.sh
@@ -1,2 +1,3 @@
 # Using descriptors, compute the taproot address at index 100 derived from this extended public key:
 #   `xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2`
+bitcoin-cli deriveaddresses "tr([00000000/86h/0h/0h]xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2/0/100)#qvyx39c6" | jq --raw-output .[0]
