c8f3268a1b0b047fd23fa71f947174b2f9af69b7
jaoleal <jgleal@protonmail.com>
2024-11-24 15:29:17 -0300

    008 better

--- submission/008.sh
+++ submission/008.sh
@@ -1,8 +1,5 @@
 # Which public key signed input 0 in this tx:
 #   `e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163`
 scriptsig=$(bitcoin-cli getrawtransaction e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163 true | jq -r .vin[0].txinwitness[2])
-prevtxid=$(bitcoin-cli getrawtransaction e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163 true | jq -r .vin[0].txid)
-index=$(bitcoin-cli getrawtransaction e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163 true | jq -r .vin[0].vout)
-scriptpubkey=$(bitcoin-cli getrawtransaction $prevtxid true | jq -r  .vout[0].scriptPubKey.hex)
 address=$(bitcoin-cli decodescript "$scriptsig" | jq  '.asm | split(" ") | .[1]')
 echo $address | jq -r
